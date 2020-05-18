04e728768aa271cfaad9ae54a4012600e1b11930
baltazarparra <baltazarparra@outlook.com>
2020-05-18 16:01:26 -0300

    Updates

---
+++ precache-manifest.fa73e8d47650c56deff89f4e4e04f327.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85e1f1c2b11303c24fc008d5e64dae9f",
+    "url": "/estudo/index.html"
+  },
+  {
+    "revision": "c29e36c6765efb34bedb",
+    "url": "/estudo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b074140ceefc0dd5252f",
+    "url": "/estudo/static/js/2.1f06f8cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/estudo/static/js/2.1f06f8cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c29e36c6765efb34bedb",
+    "url": "/estudo/static/js/main.37896cda.chunk.js"
+  },
+  {
+    "revision": "da8e631820371991b8f5",
+    "url": "/estudo/static/js/runtime-main.ffc95459.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/estudo/static/media/logo.5d5d9eef.svg"
+  }
+]);
